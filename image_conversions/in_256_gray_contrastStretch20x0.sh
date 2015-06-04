d61e4c3af40b1a910afd2ceeecff2b3f230926d1
Eric E Monson <emonson@cs.duke.edu>
2015-06-04 10:18:24 -0400

    added contrast stretch test image conversion script

---
+++ image_conversions/in_256_gray_contrastStretch20x0.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# --- USER-SET PARAMETERS ---
+
+# Loading base directory for image sets from server.conf file
+
+CONFIG_FILE=server.conf
+
+if [[ -f $CONFIG_FILE ]]; then
+    . $CONFIG_FILE
+else
+    echo "Error: config file can't be found."
+    echo $CONFIG_FILE
+    exit 1
+fi
+
+origdir="originals"
+origext=".JPEG"
+
+newsize=256
+newext=".jpg"
+newdir="in_${newsize}_gray_contrastStretch20x0"
+
+
+# --- SCRIPT ---
+
+origpath=${IMAGESETSROOTPATH}/${origdir}
+filematch="*${origext}"
+newpath=${IMAGESETSROOTPATH}/${newdir}
+
+# Create output directory if it doesn't exist
+# (isn't strictly necessary as the first set mkdir call will create this root directory,
+# too, if it doesn't exist)
+mkdir -p ${newpath}
+
+# count the number of sets
+setcount=`find $origpath -maxdepth 1 -type d | wc -l`
+sc=$((setcount - 1))
+ss=0
+
+# Find all subdirectories of origdir, each of which will be a category label
+for setpath in ${origpath}/*/; do
+    setname=`basename ${setpath}`
+    ss=$((ss + 1))
+    
+    # count number of image files in this set
+    filecount=`find $origpath/$setname -maxdepth 1 -name "$filematch" | wc -l`
+    fc=$((filecount + 0))
+    ii=0
+    
+    # create new set directory if it doesn't exist
+    mkdir -p "${newpath}/${setname}"
+    for filename in ${origpath}'/'${setname}'/'${filematch}; do
+        ii=$((ii + 1))
+        bn=`basename ${filename} ${origext}`
+        outname="${newpath}/${setname}/${bn}${newext}"
+        convert ${filename} -colorspace Gray \
+                             -resize ${newsize}x${newsize}^ \
+                             -gravity center -extent ${newsize}x${newsize} \
+                             -contrast-stretch 20x0% \
+                ${outname}
+        echo "${setname}  ${ss}/${sc}  ${ii}/${fc}  ${outname}"
+    done
+done
